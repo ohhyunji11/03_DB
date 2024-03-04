2ff0ba24af00da4c046eb2889da8da86a50e9cc2
ohhyunji <ohhyunji11@gmail.com>
2024-03-04 17:51:22 +0900

    0304 자바수업

--- 00_사용자_계정_생성.sql
+++ 00_사용자_계정_생성.sql
@@ -1,24 +1,23 @@
-
 -- 한 줄 주석
 
-
-/*
+/* 
  * 범위주석
  * 
  * */
 
--- 11G 버전 이전의 문법을 사용 가능하도록  함
+-- 11G 버전 이전의 문법을 사용 가능하도록 함
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- CTRL + ENTER : 선택한 SQL 수행
 
 
 -- 사용자 계정 생성
 CREATE USER workbook IDENTIFIED BY workbook;
---DROP USER workbook cascade;
+
 
 -- 사용자 계정에 권한 부여
 GRANT RESOURCE, CONNECT TO workbook;
 
 -- 객체가 생성될 수 있는 공간 할당량 지정
 ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
 
--- 01_SELECT.sql
+++ 01_SELECT.sql
@@ -5,9 +5,9 @@ SELECT * FROM EMPLOYEE;
  * 이때, 조회된 행들의 집합을 "RESULT SET" (조회 결과의 집합) 이라고 함.
  * 
  * - RESULT SET은 0개 이상의 행을 포함할 수 있다.
- *  왜 0개? 조건에 맞는 행이 없을 수도 있어서..
+ * 왜 0개? 조건에 맞는 행이 없을 수도 있어서..
  * 
- */
+ * */
 
 -- [작성법]
 -- SELECT 컬럼명 FROM 테이블명;
@@ -19,40 +19,44 @@ SELECT * FROM EMPLOYEE;
 SELECT EMP_ID, EMP_NAME, PHONE FROM EMPLOYEE;
 
 
--------------------------------------------------------------------
+-------------------------------------------------------------
+
 -- <컬럼 값 산술 연산>
 -- 컬럼값 : 테이블 내 한 칸(== 한 셀)에 작성된 값(DATA)
 
- -- EMPLOYEE  테이블에서 모든 사원의 사번, 이름, 급여, 연봉 조회
+-- EMPLOYEE 테이블에서 모든 사원의 사번, 이름, 급여, 연봉(급여 * 12) 조회
+SELECT EMP_ID, EMP_NAME, SALARY, SALARY * 12 
+FROM EMPLOYEE;
 
-
-SELECT EMP_ID, EMP_NAME, SALARY, SALARY * 12 FROM EMPLOYEE;
 
 SELECT EMP_NAME + 10 FROM EMPLOYEE;
 -- ORA-01722: 수치가 부적합합니다
 -- 산술 연산은 숫자(NUMBER 타입)만 가능하다!
 
----------------------------------------------------------------------
+------------------------------------------------------------------
 
 -- 날짜(DATE) 타입 조회
 
 -- EMPLOYEE 테이블에서 이름, 입사일, 오늘 날짜 조회
-SELECT EMP_NAME, HIRE_DATE, SYSDATE FROM EMPLOYEE;
+SELECT EMP_NAME, HIRE_DATE, SYSDATE
+FROM EMPLOYEE;
 
 -- SYSDATE : 시스템상의 현재 시간(날짜)를 나타내는 상수
+
 
 -- 현재 시간만 조회하기
 SELECT SYSDATE FROM DUAL;
 
 -- DUAL(DUmmy tAbLe) 테이블 : 가짜 테이블(임시 조회용 테이블)
 
+
 -- 날짜 + 산술 연산(+, -)
-SELECT SYSDATE -1, SYSDATE, SYSDATE + 1
+SELECT SYSDATE - 1, SYSDATE, SYSDATE + 1 
 FROM DUAL;
 
 -- 날짜에 +/- 연산 시 일 단위로 계산이 진행된다!
 
--------------------------------------------------------------------
+-----------------------------------------------------------------
 
 -- <컬럼 별칭 지정>
 
@@ -67,12 +71,12 @@ FROM DUAL;
  * 
  * */
 
-SELECT SYSDATE -1 "하루 전",  SYSDATE AS 현재시간, SYSDATE + 1 내일
+
+SELECT SYSDATE - 1 "하루 전", SYSDATE AS 현재시간, SYSDATE + 1 내일
 FROM DUAL;
 
 
---------------------------------------------------------------------------
-
+------------------------------------------------------------
 
 -- JAVA 리터럴 : 값 자체를 의미
 
@@ -80,33 +84,30 @@ FROM DUAL;
 --> (필수) DB의 리터럴 표기법은 '' 홑따옴표
 
 
-
 SELECT EMP_NAME, SALARY, '원 입니다'
 FROM EMPLOYEE;
 
 
+---------------------------------------------------------------------
 
----------------------------------------------------------------------------
-
--- DISTINCT : 조회 시 컬럼에 포함된 중복 값을 한 번만 포기
+-- DISTINCT : 조회 시 컬럼에 포함된 중복 값을 한 번만 표기
 
 -- 주의사항 1) DISTINCT 구문은 SELECT 마다 딱 한번 씩만 작성 가능
 -- 주의사항 2) DISTINCT 구문은 SELECT 제일 앞에 작성되어야 한다.
 
 SELECT DISTINCT DEPT_CODE, JOB_CODE FROM EMPLOYEE;
 
--------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 
 -- SELECT 컬럼명 FROM 테이블명 WHERE 조건절;
 
--- 3. SELECT절 : SELECT 컬럼명 
+-- 3. SELECT절 : SELECT 컬럼명
 -- 1. FROM절 : FROM 테이블명
 -- 2. WHERE절 : WHERE 컬럼명 연산자 값; (조건절)
 --SELECT * FROM EMPLOYEE WHERE EMP_ID = 300;
 
 -- EMPLOYEE 테이블에서 급여가 3백만원 초과인 사원의
 -- 사번, 이름, 급여, 부서코드를 조회
-
 SELECT EMP_ID, EMP_NAME, SALARY, DEPT_CODE
 FROM EMPLOYEE
 WHERE SALARY > 3000000;
@@ -119,7 +120,7 @@ WHERE DEPT_CODE = 'D9'; -- 비교연산자 ( = ) / 대입연산자 ( := )
 
 -- 비교연산자 : >, <, >=, <=, = (같다), !=, <> (같지 않다)
 
-------------------------------------------------------------------------
+---------------------------------------------------------------
 
 -- 논리연산자 (AND, OR)
 
@@ -127,8 +128,9 @@ WHERE DEPT_CODE = 'D9'; -- 비교연산자 ( = ) / 대입연산자 ( := )
 -- 사번, 이름, 급여, 전화번호 조회
 
 SELECT EMP_ID, EMP_NAME, SALARY, PHONE
-FROM EMPLOYEE 
+FROM EMPLOYEE
 WHERE SALARY < 3000000 OR SALARY >= 5000000;
+
 
 -- EMPLOYEE 테이블에서 급여가 300만 이상 500만 미만인 사원의
 -- 사번, 이름, 급여, 전화번호 조회
@@ -136,7 +138,7 @@ SELECT EMP_ID, EMP_NAME, SALARY, PHONE
 FROM EMPLOYEE
 WHERE SALARY >= 3000000 AND SALARY < 5000000;
 
-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------
 
 -- BETWEEN A AND B : A 이상 B 이하
 
@@ -146,11 +148,12 @@ FROM EMPLOYEE
 WHERE SALARY BETWEEN 3000000 AND 6000000;
 
 
--- NOT BETWEEN A AND B; A 이상 B 이하가 아닌 경우 --> A 미만 B 초과
+-- NOT BETWEEN A AND B : A 이상 B 이하가 아닌 경우 --> A 미만, B 초과
 
 SELECT EMP_ID, EMP_NAME, SALARY
 FROM EMPLOYEE
 WHERE SALARY NOT BETWEEN 3000000 AND 6000000;
+
 
 -- 날짜(DATE)에 BETWEEN 사용하기
 -- EMPLOYEE 테이블에서 입사일이 1990-01-01 ~ 1999-12-31 사이인 직원의
@@ -162,13 +165,11 @@ WHERE HIRE_DATE BETWEEN '1990-01-01' AND '1999-12-31';
 -- 오라클은 데이터 타입이 달라도 형태가 일치하면 자동으로 타입을 변환시킴
 
 -- ex) 1 = '1'
-SELECT '같음'
-FROM DUAL 
+SELECT '같음' 
+FROM DUAL
 WHERE 1 = '1';
 
----------------------------------------------------------------------------------
-
-
+--------------------------------------------------------------------
 
 -- LIKE
 -- 비교하려는 값이 특정한 패턴을 만족시키면 조회하는 연산자
@@ -176,11 +177,9 @@ WHERE 1 = '1';
 -- [작성법]
 -- WHERE 컬럼명 LIKE '패턴이 적용된 값'
 
-
--- LIKE 패턴을 나타내는 문자
+-- LIKE의 패턴을 나타내는 문자
 --> '%' : 포함
 --> '_' : 글자 수
-
 
 
 -- '%' 예시
@@ -189,13 +188,11 @@ WHERE 1 = '1';
 -- '%A%' : A를 포함하는 문자열
 
 
-
 -- '_' 예시
 -- 'A_' : A로 시작하는 두 글자 문자열
 -- '___A' : A로 끝나는 네 글자 문자열
 -- '__A__' : 세 번째 문자가 A인 다섯 글자 문자열
 -- '_____' : 다섯 글자 문자열
-
 
 -- EMPLOYEE 테이블에서 성이 '전'씨인 사원의 사번, 이름 조회
 SELECT EMP_ID, EMP_NAME
@@ -223,12 +220,12 @@ SELECT EMP_NAME, EMAIL
 FROM EMPLOYEE
 WHERE EMAIL LIKE '____%';
 -- 문제점 : _를 기준점으로 삼았으나, 패턴 중 '_'와 동일한 표기법으로 작성되어
---					구분되지 않음
+--         구분되지 않음
 
 -- 해결 방법 : LIKE의 ESCAPE OPTION 을 이용하여 _를 구분한다
 
--- LIKE의 ESCAPE OPTION : 일반 문자로 처리할 '_' / '%' 앞에
---													아무 특수기호나 첨부해서 구분하게함
+-- LIKE의 ESCAPE OPTION : 일반 문자로 처리할 '_' / '%' 앞에 
+--											아무 특수기호나 첨부해서 구분하게함
 
 
 SELECT EMP_NAME, EMAIL
@@ -256,10 +253,10 @@ AND SALARY >= 2700000;
 
 /*
  * 0. ()
- * 1. 산술연산자 (+ - * / ) 
- * 2. 연결연산자 (||)
- * 3. 비교연산자 ( > < <= >= = != <>)
- * 4. IS NULL / IS NOT NULL, LIKE, IN/ NOT IN 
+ * 1. 산술연산자 ( + - * / )
+ * 2. 연결연산자 ( || )
+ * 3. 비교연산자 ( > < <= >= = != <> )
+ * 4. IS NULL / IS NOT NULL, LIKE, IN / NOT IN
  * 5. BETWEEN AND / NOT BETWEEN AND
  * 6. NOT (논리연산자)
  * 7. AND (논리연산자)
@@ -267,64 +264,61 @@ AND SALARY >= 2700000;
  * 
  * */
 
-
-----------------------------------------------------------------------------------------
+----------------------------------------------------------
 
 /* IN 연산자
  * 
- * - 비교하려는 값과 목로겡 작성된 값 중
+ * - 비교하려는 값과 목록에 작성된 값 중
  * 일치하는 것이 있으면 조회하는 연산자
  * 
  * [작성법]
  * 
  * WHERE 컬럼명 IN(값1, 값2, 값3...)
  * 
- * WHERE 컬럼명 = 값1 
- * OR		 컬럴명 = 값2
- * OR		 컬럼명 = 값3
+ * (위와 같은 구문)
+ * WHERE 컬럼명 = 값1
+ * OR    컬렴명 = 값2
+ * OR    컬렴명 = 값3
  * ...
  * */
 
--- EMPLOYEE 테이블에서
+
+-- EMPLOYEE 테이블에서 
 -- 부서코드가 D1, D6, D9인 사원의
 -- 사번, 이름, 부서코드 조회
-
 SELECT EMP_ID, EMP_NAME, DEPT_CODE
-FROM EMPLOYEE 
+FROM EMPLOYEE
 WHERE DEPT_CODE IN('D1', 'D6', 'D9'); -- 9 명
 
-
 /*
- * WEHERE DEPT_CODE = 'D1'
- * OR DEPT_CODE = 'D6'
- * OR DEPT_CODE = 'D9';
- * 
- 
+ * WHERE DEPT_CODE = 'D1'
+ * OR  DEPT_CODE = 'D6'
+ * OR  DEPT_CODE = 'D9';
  * 
  * */
 
+
+
 /* NOT IN */
 
-
--- EMPLOYEE 테이블에서
--- 부서코드가 D1, D6, D9인 사원의
+-- EMPLOYEE 테이블에서 
+-- 부서코드가 D1, D6, D9이 아닌 사원의
 -- 사번, 이름, 부서코드 조회
-
 SELECT EMP_ID, EMP_NAME, DEPT_CODE
-FROM EMPLOYEE 
-WHERE DEPT_CODE NOT IN('D1', 'D6', 'D9')
+FROM EMPLOYEE
+WHERE DEPT_CODE NOT IN('D1', 'D6', 'D9') -- 12
 OR DEPT_CODE IS NULL; -- 부서 코드가 없는 2명 (NULL 처리 연산자)
+
 
 ----------------------------------------------------------------
 
-/*
- * NULL 처리 연산자
+/* NULL 처리 연산자
  * 
  * JAVA 에서 NULL : 참조하는 객체가 없음을 의미하는 값
- * DB 에서 NULL : 컬럼에 값이 없음을 의미하는 값
+ * DB 에서   NULL : 컬럼에 값이 없음을 의미하는 값
  * 
  * 1) IS NULL : NULL 인 경우 조회
- * 2) IS NOT NULL : NULL 이 아닌 경우
+ * 2) IS NOT NULL : NULL 이 아닌 경우 조회
  * 
  * 
  * */
@@ -332,30 +326,31 @@ OR DEPT_CODE IS NULL; -- 부서 코드가 없는 2명 (NULL 처리 연산자)
 -- EMPLOYEE 테이블에서 보너스가 있는 사원의 이름, 보너스 조회
 SELECT EMP_NAME, BONUS
 FROM EMPLOYEE
-WHERE BONUS IS NOT NULL;
+WHERE BONUS IS NOT NULL; -- 9행
+
 
 -- EMPLOYEE 테이블에서 보너스가 없는 사원의 이름, 보너스 조회
 SELECT EMP_NAME, BONUS
 FROM EMPLOYEE
 WHERE BONUS IS NULL; -- 14행
 
-------------------------------------------------------
+
+--------------------------------------------------------
+
 
 /* ORDER BY 절
  * 
  * - SELECT 문의 조회 결과(RESULT SET)를 정렬할 때 사용하는 구문
  * 
- * ** SELECT문 해석 시 가장 마지막에 해석된다!!
+ * ** SELECT문 해석 시 가장 마지막에 해석된다!!!
  * 
  * 3. SELECT절
  * 1. FROM절
  * 2. WHERE절
- * 4. ORDER BY 컬럼명 || 별칭 || 컬럼 순서  [ASC : DESC] [NULLS FIRST | LAST]
- *                               (오름차순:기본값) (내림차순)
- * 															 
- *
+ * 4. ORDER BY 컬럼명 | 별칭 | 컬럼순서 [ACS | DESC] [NULLS FIRST | LAST]
+ * 												`(오름차순:기본값) (내림차순)
+ * 
  * */
-
 
 -- EMPLOYEE 테이블에서 급여 오름차순으로
 -- 사번, 이름, 급여 조회
@@ -363,14 +358,16 @@ SELECT EMP_ID, EMP_NAME, SALARY
 FROM EMPLOYEE
 ORDER BY SALARY; -- ASC가 기본값
 
+
 -- EMPLOYEE 테이블에서
--- 급여 200만 이상의 사원의
+-- 급여 200만 이상인 사원의
 -- 사번, 이름, 급여 조회
--- 단, 급여 내림  차순으로 조회
+-- 단, 급여 내림 차순으로 조회
 SELECT EMP_ID, EMP_NAME, SALARY
 FROM EMPLOYEE
 WHERE SALARY >= 2000000
 ORDER BY 3 DESC;
+
 
 -- 입사일 순서대로 이름, 입사일 조회 (별칭 사용)
 SELECT EMP_NAME 이름, HIRE_DATE 입사일
@@ -385,11 +382,5 @@ SELECT EMP_NAME, DEPT_CODE, SALARY
 FROM EMPLOYEE
 ORDER BY DEPT_CODE, SALARY DESC;
 
-
-
-
-
-
-GROUP BY
 
 
